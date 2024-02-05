78f05071066fc62d33882f2499cbb47f750921cb
Francielle <franmkt2013@gmail.com>
2024-02-05 10:54:48 -0300

    Alert, prompt, if, else... @FranhSilva

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 alert("Bem vindo ao jogo do número secreto, vamos começar?");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+
+if(chute == numeroSecreto){
+    alert(`Parabéns, você descobriu o número secreto (${numeroSecreto})`);
+} else{
+    alert("Você errou :(");
+}
